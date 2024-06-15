e6ffcc0f29042f0536cf4f5f761402384a34c1f1
GiseleAlves <robison.carvalho12@gmail.com>
2024-06-15 15:56:13 +0100

    Desafio da variável

---
+++ JavaScript/my_script.js
@@ -0,0 +1,29 @@
+var a = 10
+var b = 20
+var c = null
+
+document.write('A:' + a + '<br />')
+document.write('A:' + b + '<br />')
+document.write('A:' + c + '<br />')
+
+document.write('<hr />')
+
+// lógica de inversão dos valores das variáveis A e B 
+
+console.log(a)
+
+a = 20
+
+console.log(a)
+
+console.log(b)
+
+b = 10
+
+console.log(b)
+
+
+
+document.write('A:' + a + '<br />')
+document.write('A:' + b + '<br />')
+document.write('A:' + c + '<br />')
